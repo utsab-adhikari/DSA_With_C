9764a512ab9ab7c3d1144d73047bf2cad1ec92fb
Utsab <utsab1adhikari@gmail.com>
2025-11-16 05:10:45 +0545

    Fibonacci by recursion

---
+++ C_Revision/P07_Fibonacci.c
@@ -0,0 +1,38 @@
+// WAP to find Fibonacci series up to n terms.
+#include <stdio.h>
+
+void fibo(int n)
+{
+    int a = 0, b = 1, c;
+
+    if (n <= 0)
+        return;
+
+    printf("%d\t", a);
+
+    if (n == 1)
+        return;
+
+    printf("%d\t", b);
+
+    for (int i = 3; i <= n; i++)
+    {
+        c = a + b;
+        printf("%d\t", c);
+        a = b;
+        b = c;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter how many terms: ");
+    scanf("%d", &num);
+
+    fibo(num);
+
+    return 0;
+}
