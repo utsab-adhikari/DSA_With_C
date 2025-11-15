1fed9f8043cd47685e450071a96543874582acd6
Utsab <utsab1adhikari@gmail.com>
2025-11-15 16:11:01 +0545

    Prime Composite Added

---
+++ C_Revision/P02_PrimeOrNot.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    int num, i, temp;
+
+    printf("\nEnter a Number:");
+    scanf("%d", &num);
+
+    temp = num;
+
+    for(i=2;i<=num; i++) {
+            if(num%i==0) {
+                break;
+            }
+    }
+
+    if(i == num ) {
+        printf("\nThe Number %d is Prime.\n", num);
+    } else {
+        printf("\nThe Number %d is Composite.\n", num);
+    }
+
+    return 0;
+}
