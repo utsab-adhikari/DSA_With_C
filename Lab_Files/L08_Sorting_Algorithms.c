50ef11259fff2acb1d46edfd6d3523414297d5ee
Utsab <utsab1adhikari@gmail.com>
2026-01-21 08:01:11 +0545

    L08 added

--- Lab_Files/L08_Sorting_Algorithms.c
+++ Lab_Files/L08_Sorting_Algorithms.c
@@ -16,14 +16,61 @@ void insertionSort(int arr[], int n) {
     }
 }
 
+void bubbleSort(int arr[], int n) {
+	int swapped=1, j=n, k=0;
+	while(swapped) {
+		swapped = 0;	
+   		 for(int i = 1;i<j;i++) {
+			if(arr[i-1]>arr[i]) {
+				int temp = arr[i-1];
+				arr[i-1] = arr[i];
+				arr[i] = temp;
+				swapped = 1;
+				k=i;
+			}
+		}
+		j = k;
+	}
+}
+
+void quickSort(int arr[], int n) {
+    if(n<=1) {
+        return;
+    }
+    int pivot = arr[0];
+    int rightSubarray[10], leftSubarray[10], r=0,l=0;
+    for(int i=1;i<n;i++) {
+        if(arr[i] > pivot) {
+            rightSubarray[r++] = arr[i];
+        } else {
+            leftSubarray[l++] = arr[i];
+        }
+    }
+    
+    quickSort(leftSubarray, l);
+    quickSort(rightSubarray, r);
+    
+    int k =0;
+    for(int i=0;i<l;i++) {
+        arr[k++] = leftSubarray[i];
+    }
+    
+    arr[k++] = pivot;
+    for(int i=0;i<r;i++) {
+        arr[k++] = rightSubarray[i];
+    }
+    
+}
+
+
+
 int main() {
-    int input[5] = {3, 4, 6, 5, 2};
+    int input[11] = {9, 4, 6, 5, 2, 8, 9, 10, 16, 17, 2};
 
-    insertionSort(input, 5);
+    quickSort(input, 11);
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 11; i++) {
         printf("%d ", input[i]);
     }
-
     return 0;
 }
